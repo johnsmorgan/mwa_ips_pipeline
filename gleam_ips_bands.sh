ad7a9781860e4ce789d8f851f2aeb7bea65c180c
John Morgan <john.morgan@icrar.org>
2018-07-19 10:54:47 +0800

    minor

--- gleam_ips_bands.sh
+++ gleam_ips_bands.sh
@@ -3,14 +3,14 @@
 # take standard GLEAM catalogue and produce peak fluxes for IPS bands
 # delete almost all columns
 
-#table_in=/data/catalogs/GLEAM.fits#2
-#table_out=gleam_ips_bands.fits
-#stilts tpipe \
-#	in=$table_in \
-#	out=$table_out \
-#	cmd='addcol Fp080 (Fp076*pow(e_Fp076,-2)+Fp084*pow(e_Fp084,-2))/(pow(e_Fp076,-2)+pow(e_Fp084,-2))' \
-#	cmd='addcol Fp162 (Fp158*pow(e_Fp158,-2)+Fp166*pow(e_Fp166,-2))/(pow(e_Fp158,-2)+pow(e_Fp166,-2))' \
-#	cmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp080 Fp162"'
+table_in=/data/catalogs/GLEAM.fits#2
+table_out=gleam_ips_bands.fits
+stilts tpipe \
+	in=$table_in \
+	out=$table_out \
+	cmd='addcol Fp080 (Fp076*pow(e_Fp076,-2)+Fp084*pow(e_Fp084,-2))/(pow(e_Fp076,-2)+pow(e_Fp084,-2))' \
+	cmd='addcol Fp162 (Fp158*pow(e_Fp158,-2)+Fp166*pow(e_Fp166,-2))/(pow(e_Fp158,-2)+pow(e_Fp166,-2))' \
+	cmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp080 Fp162"'
 
 stilts tmatch2 \
 	in1=$table_out \
