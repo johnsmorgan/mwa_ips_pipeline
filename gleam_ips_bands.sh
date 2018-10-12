903789f29c3e755ab939d769ef3397ce80b6a5ab
John Morgan <john.morgan@icrar.org>
2018-10-12 16:30:29 +0800

    fix up

--- gleam_ips_bands.sh
+++ gleam_ips_bands.sh
@@ -5,28 +5,34 @@
 
 table_in=/data/catalogs/GLEAM.fits#2
 table_out=gleam_ips_bands.fits
+callingham_path=/data/catalogs/callingham2017
 stilts tpipe \
 	in=$table_in \
 	out=$table_out \
 	cmd='addcol Fp080 (Fp076*pow(e_Fp076,-2)+Fp084*pow(e_Fp084,-2))/(pow(e_Fp076,-2)+pow(e_Fp084,-2))' \
 	cmd='addcol Fp162 (Fp158*pow(e_Fp158,-2)+Fp166*pow(e_Fp166,-2))/(pow(e_Fp158,-2)+pow(e_Fp166,-2))' \
-	cmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp080 Fp162"'
+	cmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp080 Fp162 alpha e_alpha"'
 
 stilts tmatch2 \
 	in1=$table_out \
-	in2=high_low_freq_peakedspectrum_table.fits \
+	in2=$callingham_path/high_low_freq_peakedspectrum_table.fits \
 	out=$table_out \
-	icmd2="keepcols Gleam_name nu_p nu_p_error" \
-	matcher=ExactValue\
+	join=all1 \
+	icmd2='keepcols "Gleam_name nu_p nu_p_error"' \
+	icmd2="replacecol Gleam_name substring(Gleam_name,6)" \
+	matcher=exact\
 	values1="GLEAM" \
 	values2="Gleam_name" \
+	ocmd="delcols Gleam_name"
 
 stilts tmatch2 \
 	in1=$table_out \
-	in2=gps_sample_peakedspectrum_table.fits \
+	in2=$callingham_path/gps_sample_peakedspectrum_table.fits \
 	out=$table_out \
+	join=all1 \
 	icmd2="keepcols Gleam_name" \
-	matcher=ExactValue\
+	icmd2="replacecol Gleam_name substring(Gleam_name,6)" \
+	matcher=exact\
 	values1="GLEAM" \
 	values2="Gleam_name" \
 	ocmd="addcol gps !NULL_Gleam_name" \
@@ -34,10 +40,12 @@ stilts tmatch2 \
 
 stilts tmatch2 \
 	in1=$table_out \
-	in2=srcs_peak_below72MHz.fits \
+	in2=$callingham_path/srcs_peak_below72MHz.fits \
 	out=$table_out \
+	join=all1 \
 	icmd2="keepcols Gleam_name" \
-	matcher=ExactValue\
+	icmd2="replacecol Gleam_name substring(Gleam_name,6)" \
+	matcher=exact\
 	values1="GLEAM" \
 	values2="Gleam_name" \
 	ocmd="addcol peak_below_72mhz !NULL_Gleam_name" \
@@ -45,10 +53,12 @@ stilts tmatch2 \
 
 stilts tmatch2 \
 	in1=$table_out \
-	in2=convex_src_table.fits \
+	in2=$callingham_path/convex_src_table.fits \
 	out=$table_out \
+	join=all1 \
 	icmd2="keepcols Gleam_name" \
-	matcher=ExactValue\
+	icmd2="replacecol Gleam_name substring(Gleam_name,6)" \
+	matcher=exact\
 	values1="GLEAM" \
 	values2="Gleam_name" \
 	ocmd="addcol convex !NULL_Gleam_name" \
