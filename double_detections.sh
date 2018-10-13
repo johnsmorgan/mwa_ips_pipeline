0554d234983e1ef429eaa194ba413bec99889970
John Morgan <john.morgan@icrar.org>
2018-10-13 16:54:36 +0800

    save file containging non-double-detections

--- double_detections.sh
+++ double_detections.sh
@@ -8,6 +8,17 @@ stilts tmatch2 \
         suffix1="_cont" suffix2="_scint" \
 	find="best2" \
         params=60 \
-        out=$3 \
+        out=$3
         # ocmd='select snr_cont>5||snr_scint>5' \
 
+stilts tmatch2 \
+        in1=$1 \
+        in2=$2 \
+        matcher=skyEllipse \
+        values1="ra dec a b pa" \
+        values2="ra dec a b pa" \
+        suffix1="_cont" suffix2="_scint" \
+	find="best2" \
+        params=60 \
+	join=2not1 \
+        out=$4 \
