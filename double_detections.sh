817723261d39826bb0b94ffb7f8dfe7f5446775d
John Morgan <john.morgan@icrar.org>
2018-10-13 17:10:55 +0800

    updated stilts code to save only useful columns and save metadata

--- double_detections.sh
+++ double_detections.sh
@@ -2,14 +2,20 @@
 stilts tmatch2 \
         in1=$1 \
         in2=$2 \
+	icmd1="delcols 'elongation pbcor'"
         matcher=skyEllipse \
         values1="ra dec a b pa" \
         values2="ra dec a b pa" \
         suffix1="_cont" suffix2="_scint" \
 	find="best2" \
         params=60 \
+	fixcols='all' \
+        ocmd='select snr_cont>5||snr_scint>5' \
+	ocmd="colmeta -name elongation elongation_scint'"
+	ocmd="colmeta -name pbcor pbcor_scint'"
+	ocmd="addcol index dS/peak_flux_cont"
+	ocmd="colmeta -desc 'Scintillation Index' index"
         out=$3
-        # ocmd='select snr_cont>5||snr_scint>5' \
 
 stilts tmatch2 \
         in1=$1 \
@@ -17,7 +23,6 @@ stilts tmatch2 \
         matcher=skyEllipse \
         values1="ra dec a b pa" \
         values2="ra dec a b pa" \
-        suffix1="_cont" suffix2="_scint" \
 	find="best2" \
         params=60 \
 	join=2not1 \
