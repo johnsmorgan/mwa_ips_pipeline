fdbff4ababa003123b491be1b898cf78d762f516
John Morgan <john.morgan@icrar.org>
2020-02-12 14:32:36 +0800

    Allow stokes I generation

--- apply_beam.py
+++ apply_beam.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from optparse import OptionParser #NB zeus does not have argparse!
 import numpy as np
 from astropy.io import fits
@@ -6,30 +7,42 @@ from image_stack import ImageStack
 parser = OptionParser(usage = "usage:" +
     """
     python apply_beam.py \
-               hdf5_file_in.h5py fitsfile_in.fits fitsfile_out.fits [-fr]
+               hdf5_file_in.h5py fitsfile_in.fits [fitsfile_in.fits] fitsfile_out.fits [-fr]
     """)
 parser.add_option("-f", "--freq", default=None, dest="freq", help="freq")
+parser.add_option("-i", "--stokesi", default=None, dest="combine", help="combine XX and YY into stokes I")
 parser.add_option("-r", "--reverse", action='store_true', dest="reverse", help="remove an already-applied primary beam correction")
 
 opts, args = parser.parse_args()
-hdf5_in= args[0]
-fits_in= args[1]
-fits_out= args[2]
+
+hdf5_in = args[0]
+hdus = fits.open(args[1])
+in_data = hdus[0].data
+if not opts.combine:
+    fits_out = args[2]
+else:
+    hdusy = fits.open(args[2])
+    fits_out = args[3]
 
 if opts.freq is not None:
     group = opts.freq
 else:
     group = '/'
 
-df = ImageStack(hdf5_in, freq='121-132')
+if opts.freq is not None:
+    df = ImageStack(hdf5_in, freq=opts.freq)
+else:
+    df = ImageStack(hdf5_in)
 beam = df.group['beam'][..., 0, 0] # last two dimensions are frequency and time
-hdus = fits.open(fits_in)
-in_data = hdus[0].data
 n_x, n_y = in_data.shape[-1], in_data.shape[-2]
 assert n_x == beam.shape[-3] and n_y == beam.shape[-4], "beam and image xy dimensions do not match %s %s %s" % (str(beam.shape), n_x, n_y)
-if opts.reverse:
-    out_data = in_data/np.average(1/beam, weights=beam**2, axis=0)**-1
+if opt.combine:
+    in_data = hdus
+if not opts.reverse:
+    if not opts.combine:
+        out_data = in_data*np.average(1/beam, weights=beam**2, axis=0)**-1
+    else:
 else:
-    out_data = in_data*np.average(1/beam, weights=beam**2, axis=0)**-1
+    out_data = in_data/np.average(1/beam, weights=beam**2, axis=0)**-1
 hdus[0].data = out_data.reshape((1, 1, data_y, data_x))
-hdu.writeto(fits_out)
+hdus.writeto(fits_out)
