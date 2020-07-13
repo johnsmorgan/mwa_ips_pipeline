ced704840c9760a34218a1b6ea3b8b9b401ce162
John Morgan <john.morgan@icrar.org>
2020-07-13 12:51:12 +0800

    python3 and remove spurious noise calculation

--- make_cat.py
+++ make_cat.py
@@ -5,7 +5,9 @@ from astropy.table import Table
 from astropy.time import Time
 from astropy.units import deg
 from astropy.coordinates import SkyCoord, get_sun
-from image_stack import ImageStack
+from imstack import ImageStack
+from scipy.optimize import minimize
+
 
 from optparse import OptionParser, OptionValueError
 
@@ -32,7 +34,7 @@ opts, args = parser.parse_args()
 # set verbosity
 
 if not opts.pol in POL_OPTIONS:
-    raise OptionValueError, "polarisation must be one of %s" % (", ".join(POL_OPTIONS))
+    raise OptionValueError("polarisation must be one of %s" % (", ".join(POL_OPTIONS)))
 if os.path.exists(args[2]):
     os.remove(args[2])
 imstack = ImageStack(args[0], freq='121-132')
@@ -43,38 +45,42 @@ t = Table.read(args[1])
 # elongation
 if not opts.obsid:
     opts.obsid = args[0][:10]
-print "calculating elongations"
+print("calculating elongations")
 time = Time(float(opts.obsid), format='gps')
 sun = get_sun(time.utc)
 #t['elongation'] = sun.separation(SkyCoord(t['ra', 'dec']*deg))
 t['elongation'] = sun.separation(SkyCoord(t['ra'], t['dec'], unit = "deg"))
 t['snr'] = t['peak_flux'] / t['local_rms']
 
-print "calculating primary beam"
+print("calculating primary beam")
 t["pbcor"] = np.nan*np.ones(len(t))
 # loop over all unmasked values
 for s in np.argwhere(~t['ra'].mask)[:, 0]:
-    print s,
+    print(s, sep=' ')
     x, y = imstack.world2pix(t['ra'][s], t['dec'][s])
-    print x,y
+    #print(x,y)
     if x<0 or x >= dim_x:
         continue
     if y<0 or y >= dim_y:
         continue
     if opts.pol == "I":
-        t["pbcor"][s] = imstack.pix2beam(x, y, scale=False)
+        t["pbcor"][s] = imstack.pix2beam(x, y, scale=True)
     elif opts.pol == "XX":
-        t["pbcor"][s] = imstack.pix2beam(x, y, avg_pol=False, scale=False)[0]
+        t["pbcor"][s] = imstack.pix2beam(x, y, avg_pol=False, scale=True)[0]
     elif opts.pol == "YY":
-        t["pbcor"][s] = imstack.pix2beam(x, y, avg_pol=False, scale=False)[1]
+        t["pbcor"][s] = imstack.pix2beam(x, y, avg_pol=False, scale=True)[1]
+
+f = lambda x: -imstack.pix2beam(x[0], x[1], scale=True)
+min_ = minimize(f, (1200, 1200), method='Nelder-Mead', options={'xatol': 1})
+pbmax = -min_['fun']
+t["pbcor_norm"] = t["pbcor"]/pbmax
 
 if opts.var:
     t['dS'] = np.sqrt((t['peak_flux']+t['background'])**2 - t['background']**2)
     t['err_dS'] = np.sqrt((t['peak_flux']+t['background']+t['local_rms'])**2 - t['background']**2) - t['dS']
-    t['noise'] = np.sqrt((t['peak_flux']+t['background'])**2 - t['peak_flux']**2)
-    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'uuid', 'dS', 'err_dS', 'local_rms', 'noise', 'snr'])
+    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'dS', 'err_dS', 'background', 'local_rms', 'snr'])
 else:
-    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'peak_flux', 'background', 'local_rms', 'a', 'b', 'pa', 'elongation', 'pbcor', 'uuid', 'snr'])
+    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'peak_flux', 'background', 'local_rms', 'snr'])
 
-print "writing votable"
+print("writing votable")
 t.write(args[2], format='votable')
