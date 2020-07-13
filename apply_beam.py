3ce779eb3d2d4b306ad6c43f7dcbd2514caf745f
John Morgan <john.morgan@icrar.org>
2020-07-13 12:54:07 +0800

    change stokes I calculation

--- apply_beam.py
+++ apply_beam.py
@@ -17,12 +17,13 @@ opts, args = parser.parse_args()
 
 hdf5_in = args[0]
 hdus = fits.open(args[1])
-in_data = hdus[0].data
 if not opts.combine:
     fits_out = args[2]
+    in_data = hdus[0].data
 else:
     hdusy = fits.open(args[2])
     fits_out = args[3]
+    in_data = hdus[0].data, hdusy[0].data
 
 if opts.freq is not None:
     group = opts.freq
@@ -36,12 +37,12 @@ else:
 beam = df.group['beam'][..., 0, 0] # last two dimensions are frequency and time
 n_x, n_y = in_data.shape[-1], in_data.shape[-2]
 assert n_x == beam.shape[-3] and n_y == beam.shape[-4], "beam and image xy dimensions do not match %s %s %s" % (str(beam.shape), n_x, n_y)
-if opt.combine:
-    in_data = hdus
+
 if not opts.reverse:
     if not opts.combine:
         out_data = in_data*np.average(1/beam, weights=beam**2, axis=0)**-1
     else:
+        out_data = in_data/np.average(1/beam, weights=beam**2, axis=0)**-1
 else:
     out_data = in_data/np.average(1/beam, weights=beam**2, axis=0)**-1
 hdus[0].data = out_data.reshape((1, 1, data_y, data_x))
