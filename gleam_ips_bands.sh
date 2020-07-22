d11297d3b18236b565f19f46bee521360f4fb109
John Morgan <john.morgan@icrar.org>
2020-07-22 16:16:20 +0800

    add GLEAM II (some galactic areas)

--- gleam_ips_bands.sh
+++ gleam_ips_bands.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 # NB this script is not included in the makefile and must be run separately!
 
 # take standard GLEAM catalogue and produce peak fluxes for IPS bands
 # delete almost all columns
 
 table_in=/data/catalogs/GLEAM.fits#2
+table_in_gal=/data/catalogs/gleam2.fits
 table_out=gleam_ips_bands.fits
 callingham_path=/data/catalogs/callingham2017
-stilts tpipe \
+
+stilts tcat \
 	in=$table_in \
+	in=$table_in_gal \
 	out=$table_out \
-	cmd='addcol Fp080 (Fp076*pow(e_Fp076,-2)+Fp084*pow(e_Fp084,-2))/(pow(e_Fp076,-2)+pow(e_Fp084,-2))' \
-	cmd='addcol Fp162 (Fp158*pow(e_Fp158,-2)+Fp166*pow(e_Fp166,-2))/(pow(e_Fp158,-2)+pow(e_Fp166,-2))' \
-	cmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp080 Fp162 alpha e_alpha"'
+	icmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp076 e_Fp076 Fp084 e_Fp084 Fp158 e_Fp158 Fp166 e_Fp166 alpha e_alpha"' \
+	seqcol='galactic' \
+	ocmd='replacecol galactic galactic==2' \
+	ocmd='addcol Fp080 (Fp076*pow(e_Fp076,-2)+Fp084*pow(e_Fp084,-2))/(pow(e_Fp076,-2)+pow(e_Fp084,-2))' \
+	ocmd='addcol Fp162 (Fp158*pow(e_Fp158,-2)+Fp166*pow(e_Fp166,-2))/(pow(e_Fp158,-2)+pow(e_Fp166,-2))' \
+	ocmd='keepcols "GLEAM RAJ2000 DEJ2000 awide bwide pawide Fp080 Fp162 alpha e_alpha galactic"'
 
 stilts tmatch2 \
 	in1=$table_out \
