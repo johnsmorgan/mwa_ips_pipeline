fa5e515fdccffcedbf2aa889bf9fceb943706db2
John Morgan <john.morgan@icrar.org>
2021-04-19 12:02:18 +0800

    add interp code to make_cat

--- make_cat.py
+++ make_cat.py
@@ -19,6 +19,7 @@ parser = OptionParser(usage = "usage: %prog input.hdf5 input.vot output.vot" +
 add various columns and delete unneeded ones.
 """)
 parser.add_option("-v", "--variability", dest="var", action="store_true", help="Calculate variability image parameters (dS etc)")
+parser.add_option("-i", "--interp", dest="interp", action="store_true", help="Calculate interp image parameters (dS etc)")
 parser.add_option("-o", "--obsid", dest="obsid", default=None, help="time in gps format used for calculation of Sun location (default: first 10 letters of input.hdf5)")
 parser.add_option("--pol", dest="pol", default="I", help="primary beam polarisation to use: {} (default=%default)".format((", ".join(POL_OPTIONS))))
 
@@ -33,6 +34,8 @@ opts, args = parser.parse_args()
 # gpstime
 # set verbosity
 
+if opts.var and opts.interp:
+    raise OptionValueError("-v/--variability can not be set with -i/--interp")
 if not opts.pol in POL_OPTIONS:
     raise OptionValueError("polarisation must be one of %s" % (", ".join(POL_OPTIONS)))
 if os.path.exists(args[2]):
@@ -78,6 +81,10 @@ t["pbcor_norm"] = t["pbcor"]/pbmax
 if opts.var:
     t['dS'] = np.sqrt((t['peak_flux']+t['background'])**2 - t['background']**2)
     t['err_dS'] = np.sqrt((t['peak_flux']+t['background']+t['local_rms'])**2 - t['background']**2) - t['dS']
+    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'dS', 'err_dS', 'background', 'local_rms', 'snr'])
+elif opts.interp
+    t['dS'] = np.sqrt((t['peak_flux2']+t['background2'])**2 - t['background2']**2)
+    t['err_dS'] = np.sqrt((t['peak_flux2']+t['background2']+t['local_rms2'])**2 - t['background2']**2) - t['dS']
     t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'dS', 'err_dS', 'background', 'local_rms', 'snr'])
 else:
     t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'peak_flux', 'background', 'local_rms', 'snr'])
