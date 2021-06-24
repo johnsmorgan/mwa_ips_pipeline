7ecc716ca35e02c2a1042b5e3754b761eb26803e
John Morgan <mojoh81@gmail.com>
2021-06-24 05:45:44 +0000

    added in accidentally deleted stuff

--- make_cat.py
+++ make_cat.py
@@ -12,11 +12,15 @@ from optparse import OptionParser, OptionValueError
 
 POL_OPTIONS=("XX", "YY", "I")
 
-parser = OptionParser(usage = "usage: %prog input.hdf5" +
+parser = OptionParser(usage = "usage: %prog input.hdf5 input.vot output.vot" +
 """
 
 add various columns and delete unneeded ones.
 """)
+-parser.add_option("-v", "--variability", dest="var", action="store_true", help="Calculate variability image parameters (dS etc)")
+-parser.add_option("-i", "--interp", dest="interp", action="store_true", help="Calculate interp image parameters (dS etc)")
+ parser.add_option("-o", "--obsid", dest="obsid", default=None, help="time in gps format used for calculation of Sun location (default: first 10 letters of input.hdf5)")
+-parser.add_option("--pol", dest="pol", default="I", help="primary beam polarisation to use: {} (default=%default)".format((", ".join(POL_OPTIONS))))
 #parser.add_option("-m", "--moment2", dest="moment2", action="store_true", help="Calculate variability image parameters (dS etc)")
 
 opts, args = parser.parse_args()
@@ -29,6 +33,12 @@ opts, args = parser.parse_args()
 # variability
 # gpstime
 # set verbosity
+if opts.var and opts.interp:
+    raise OptionValueError("-v/--variability can not be set with -i/--interp")
+if not opts.pol in POL_OPTIONS:
+    raise OptionValueError("polarisation must be one of %s" % (", ".join(POL_OPTIONS)))
+if os.path.exists(args[2]):
+    os.remove(args[2])
 
 imstack = ImageStack(args[0], freq='121-132')
 dim_x, dim_y = imstack.group['beam'].shape[1:3]
